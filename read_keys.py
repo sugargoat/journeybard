398d89bba2c6dd6cf05988fe04834dcb546b5620
drakeley <sara.drakeley@gmail.com>
2019-11-17 19:47:15 -0800

    Read on loop

--- read_keys.py
+++ read_keys.py
@@ -2,12 +2,17 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+import time
 
 reader = SimpleMFRC522()
 
-try:
-    id, text = reader.read()
-    print(id)
-    print(text)
-finally:
-    GPIO.cleanup()
+while True:
+    try:
+        id, text = reader.read()
+        print(id)
+        print("Ah, the {} has returned. Do tell us of your journey.".format(text))
+    except Exception as e:
+        print("Unfortunately, we cannot hear the tale of {} at the moment".format(text))
+    time.sleep(3)
+
+GPIO.cleanup()
