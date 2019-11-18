b5eb62048d9653bd8341671fa97730b0d741ca26
drakeley <sara.drakeley@gmail.com>
2019-11-17 19:50:57 -0800

    Cleanup on keyboardinterrupt

--- read_keys.py
+++ read_keys.py
@@ -17,9 +17,10 @@ while True:
         print(id)
         print("Ah, the {} has returned. Do tell us of your journey.".format(text.strip()))
         welcomed = False
+    except KeyboardInterrupt:
+        print("Goodbye!")
+        GPIO.cleanup()
     except Exception as e:
         print("Unfortunately, we cannot hear the tale of {} at the moment".format(text.strip()))
         welcomed = False
     time.sleep(3)
-
-GPIO.cleanup()
