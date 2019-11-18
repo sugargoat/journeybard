6a7aaa8a2533dac67672294fe04d485be6104b11
drakeley <sara.drakeley@gmail.com>
2019-11-17 19:55:18 -0800

    name = main

--- read_keys.py
+++ read_keys.py
@@ -4,24 +4,26 @@ import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import time
 
-reader = SimpleMFRC522()
 
-welcomed = False
+if __name__ == '__main__':
+    reader = SimpleMFRC522()
 
-while True:
-    if not welcomed:
-        print("\n~=~=~=~=~=~=~=\nWelcome to the storied pages of the BardGuild. Please present your muse.\n")
-        welcomed = True
-    try:
-        id, text = reader.read()
-        print(id)
-        print("Ah, the {} has returned. Do tell us of your journey.".format(text.strip()))
-        welcomed = False
-    except KeyboardInterrupt:
-        print("Goodbye!")
-        GPIO.cleanup()
-        exit()
-    except Exception as e:
-        print("Unfortunately, we cannot hear the tale of {} at the moment".format(text.strip()))
-        welcomed = False
-    time.sleep(3)
+    welcomed = False
+
+    while True:
+        if not welcomed:
+            print("\n~=~=~=~=~=~=~=\nWelcome to the storied pages of the BardGuild. Please present your muse.\n")
+            welcomed = True
+        try:
+            id, text = reader.read()
+            print(id)
+            print("Ah, the {} has returned. Do tell us of your journey.".format(text.strip()))
+            welcomed = False
+        except KeyboardInterrupt:
+            print("Goodbye!")
+            GPIO.cleanup()
+            exit()
+        except Exception as e:
+            print("Unfortunately, we cannot hear the tale of {} at the moment".format(text.strip()))
+            welcomed = False
+        time.sleep(3)
