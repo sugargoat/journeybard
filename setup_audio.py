8d886b1b609569207f2ee4c4c77b701ea3c8cb69
drakeley <sara.drakeley@gmail.com>
2019-11-17 20:42:49 -0800

    Fix

--- setup_audio.py
+++ setup_audio.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with open("messages.json") as m:
         messages = json.load(m)
 
-    os.makedirs("audio/messages")
+    os.makedirs("audio/messages", exist_ok=True)
 
     for category, texts in messages.items():
         for i, text in enumerate(texts):
