e8c1416bea3cb2f1320de2f76e58f82daed0ac88
drakeley <sara.drakeley@gmail.com>
2019-11-17 19:49:38 -0800

    Some UX

--- read_keys.py
+++ read_keys.py
@@ -6,13 +6,20 @@ import time
 
 reader = SimpleMFRC522()
 
+welcomed = False
+
 while True:
+    if not welcomed:
+        print("\n~=~=~=~=~=~=~=\nWelcome to the storied pages of the BardGuild. Please present your muse.\n")
+        welcomed = True
     try:
         id, text = reader.read()
         print(id)
-        print("Ah, the {} has returned. Do tell us of your journey.".format(text))
+        print("Ah, the {} has returned. Do tell us of your journey.".format(text.strip()))
+        welcomed = False
     except Exception as e:
-        print("Unfortunately, we cannot hear the tale of {} at the moment".format(text))
+        print("Unfortunately, we cannot hear the tale of {} at the moment".format(text.strip()))
+        welcomed = False
     time.sleep(3)
 
 GPIO.cleanup()
